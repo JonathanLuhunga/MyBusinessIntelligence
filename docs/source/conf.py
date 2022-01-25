e4b2e25bc20307602c209556f3dcb1b4c946ccca
JonathanLuhunga <98150635+JonathanLuhunga@users.noreply.github.com>
2022-01-25 14:54:42 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BIMumiDocs'
+copyright = '2021, Jonathan Luhunga'
+author = 'Jonathan Luhunga'
 
 release = '0.1'
 version = '0.1.0'
